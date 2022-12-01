c81cd79af54488514a83286aecfc003f7ea73c9c
JohannesAlves <johannes001002@gmail.com>
2022-12-01 12:09:16 -0300

    feat: middleware verify balance account

---
+++ server/src/middleware/verifyBalanceAccount.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+
+const prisma = new PrismaClient();
+
+interface IAccountData {
+    fromAccountId: string;
+    amount: number;
+}
+
+async function verifyBalanceAccount(request: Request, response: Response, next: NextFunction) {
+    const { fromAccountId, amount }: IAccountData = request.body;
+
+    const verifyBalance = await prisma.account.findUnique({
+        where: {
+            accountNumber: fromAccountId,
+        },
+    });
+
+    console.log(verifyBalance);
+}
