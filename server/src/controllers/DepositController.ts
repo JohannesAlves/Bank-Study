f95a77f0e6d84ae2a1e39221f91917adc2f1985b
JohannesAlves <johannes001002@gmail.com>
2022-12-01 15:16:03 -0300

    feat: init deposit controller.

---
+++ server/src/controllers/DepositController.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+interface IDepositData {
+    amount: number;
+    toAccountId: string;
+}
+
+const DepositController = async (request: Request, response: Response) => {
+    const { amount, toAccountId }: IDepositData = request.body;
+
+    if (!amount || !toAccountId) {
+        return response.status(400).json({ deposit: false, message: "U can't send data empty" });
+    }
+
+    const deposit = await prisma.account.update({
+        where: {
+            accountId: toAccountId,
+        },
+        data: {
+            balance: amount,
+        },
+    });
+
+    return response.status(200).json({ deposit: true, message: "Deposit sucessful!", data: {} });
+};
