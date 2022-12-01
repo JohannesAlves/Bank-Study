d4b0175e9a0bd54462796c6dca6b6f315c5b2049
JohannesAlves <johannes001002@gmail.com>
2022-12-01 12:02:28 -0300

    feat: transfer controller

---
+++ server/src/controllers/TransferController.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+interface ITransferData {
+    fromAccountId: string;
+    toAccountId: string;
+    amount: number;
+}
+
+const TransferController = async (request: Request, response: Response) => {
+    const { fromAccountId, toAccountId, amount }: ITransferData = request.body;
+
+    try {
+        const transfer = await prisma.transfer.create({
+            data: {
+                amount,
+                fromAccountId,
+                toAccountId,
+            },
+        });
+
+        return response.status(202).json({
+            transfer: true,
+            message: "Transfer with sucessful!",
+            data: {
+                idTransfer: transfer.idTransfer,
+                fromAccountId: transfer.fromAccountId,
+                toAccountId: transfer.toAccountId,
+            },
+        });
+    } catch {
+        return response.status(500).json({ message: "Internal server error when try transfer amount." });
+    }
+};
