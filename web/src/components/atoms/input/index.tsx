d58a9ef5217d146477be09c448ac9871e3b5f019
JohannesAlves <johannes001002@gmail.com>
2022-12-02 18:08:24 -0300

    chore: add placeholder in input

--- web/src/components/atoms/input/index.tsx
+++ web/src/components/atoms/input/index.tsx
@@ -1,16 +1,17 @@
 type Props = {
-    handleOnChange: React.ChangeEventHandler<HTMLInputElement>;
+    handleOnChange?: React.ChangeEventHandler<HTMLInputElement>;
     value?: string;
+    placeholder?: string;
 };
 
-function Input({ handleOnChange, value }: Props) {
+function Input({ handleOnChange, value, placeholder }: Props) {
     return (
         <input
             name="name"
             onChange={handleOnChange}
             autoComplete="off"
             value={value}
-            placeholder="123.456.789-10"
+            placeholder={placeholder}
             className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
         />
     );
