56f9be912baee2fed3df815ecb59c48428aa10bb
JohannesAlves <johannes001002@gmail.com>
2022-12-02 01:00:22 -0300

    chore: molecule titles main

---
+++ web/src/components/molecules/TitlesMain/TitlesMain.tsx
@@ -0,0 +1,13 @@
+import { Subtitle } from "../../atoms/subtitle";
+import { Title } from "../../atoms/title";
+
+function TitlesMain() {
+    return (
+        <div>
+            <Title title="👋 Olá, futuro(a) parceiro(a) de equipe" />
+            <Subtitle subtitle="Me informe o seu CPF ;)" />
+        </div>
+    );
+}
+
+export default TitlesMain;
