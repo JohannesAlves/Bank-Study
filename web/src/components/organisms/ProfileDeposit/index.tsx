cf359c70aa014ba4f85bbb27808793ca1319274e
JohannesAlves <johannes001002@gmail.com>
2022-12-02 18:08:47 -0300

    feat: profile deposit

--- web/src/components/organisms/ProfileDeposit/index.tsx
+++ web/src/components/organisms/ProfileDeposit/index.tsx
@@ -1,7 +1,21 @@
+import { Button } from "../../atoms/Button";
+import Input from "../../atoms/Input";
+
 export function ProfileDeposit() {
     return (
         <>
-            <h1>oi</h1>
+            <div className=" w-9/12 h-4/5 mx-auto mt-10 rounded-md">
+                <div className="flex flex-col justify-center items-center mt-8">
+                    <div>
+                        <h2 className="text-3xl text-slate-300 font-bold"></h2>
+                    </div>
+
+                    <div>
+                        <Input />
+                    </div>
+                    <Button btnText="DEPOSITAR" />
+                </div>
+            </div>
         </>
     );
 }
