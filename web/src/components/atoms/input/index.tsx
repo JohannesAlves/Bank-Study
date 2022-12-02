7c53403b2f29224a4d9632f40a0a90fc52e52391
JohannesAlves <johannes001002@gmail.com>
2022-12-02 11:17:04 -0300

    chore: add autocomplete OFF in input.

--- web/src/components/atoms/input/index.tsx
+++ web/src/components/atoms/input/index.tsx
@@ -8,6 +8,7 @@ function Input({ handleOnChange, value }: Props) {
         <input
             name="name"
             onChange={handleOnChange}
+            autoComplete="off"
             value={value}
             placeholder="123.456.789-10"
             className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
