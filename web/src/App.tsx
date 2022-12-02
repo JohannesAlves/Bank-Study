a16856c176c5283b2e64ae0d2a6b4a1ddd2e672c
JohannesAlves <johannes001002@gmail.com>
2022-12-02 12:04:41 -0300

    feat: profile with btns user

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import Main from "./pages/Main";
+import Profile from "./pages/Profile";
 
 function App() {
     return (
         <div className="bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
-            <div className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
-                <Main />
+            <div className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
+                <Profile />
             </div>
         </div>
     );
---
+++ web/src/pages/Profile.tsx
@@ -0,0 +1,39 @@
+import { Subtitle } from "../components/atoms/subtitle";
+import { Title } from "../components/atoms/title";
+
+function Profile() {
+    return (
+        <>
+            <div className="w-1/6 h-full bg-slate-200 rounded-lg flex flex-col ">
+                {/* avatar img */}
+                <div className="overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mt-3 mx-auto">
+                    <svg
+                        className="absolute -left-1 w-13 h-12 text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                            clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                </div>
+
+                <div className="text-center">
+                    <h4>Johannes Oliveira Alves</h4>
+                </div>
+
+                <div className="flex flex-col space-y-4 mt-10 text-lg">
+                    <button className="hover:bg-orange-500">Início</button>
+                    <button className="hover:bg-orange-500">Histórico</button>
+                    <button className="hover:bg-orange-500">Depositar</button>
+                    <button className="hover:bg-orange-500">Transferir</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Profile;
