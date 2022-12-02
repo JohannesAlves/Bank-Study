be228860e7eebf77301988299e162cce46fd9465
JohannesAlves <johannes001002@gmail.com>
2022-12-02 13:34:00 -0300

    chore: remove unused code.

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,7 +1,3 @@
-import { Subtitle } from "../components/atoms/subtitle";
-import { Title } from "../components/atoms/title";
-import AvatarAndName from "../components/molecules/AvatarAndName/AvatarAndName";
-import ButtonsNav from "../components/molecules/ButtonsNav/ButtonsNav";
 import NavOrganism from "../components/organisms/NavOrganism/NavOrganism";
 
 function Profile() {
@@ -15,8 +11,6 @@ function Profile() {
                         Saldo disponível: <br />
                         R$200,00
                     </h3>
-
-                    <h3></h3>
                 </div>
             </div>
         </>
