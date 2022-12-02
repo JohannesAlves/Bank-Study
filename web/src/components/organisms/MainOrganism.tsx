bdc2e7ecb8ab4d5a5520340e408dace7257910a6
JohannesAlves <johannes001002@gmail.com>
2022-12-02 11:12:10 -0300

    feat: main organism and put component in main.

---
+++ web/src/components/organisms/MainOrganism.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { maskCPF } from "../../utils/maskCPF";
+import { ButtonAndInput } from "../molecules/ButtonAndInput/ButtonAndInput";
+import TitlesMain from "../molecules/TitlesMain/TitlesMain";
+
+function MainOrganism() {
+    const [value, setValue] = useState("");
+
+    function handleChangeMask(event: React.FormEvent<HTMLInputElement>) {
+        const { value } = event.currentTarget;
+
+        setValue(maskCPF(value));
+    }
+
+    return (
+        <div className="flex flex-col w-full h-full	items-center">
+            <TitlesMain />
+            <ButtonAndInput btnText="VAMOS LÁ!" handleOnChange={handleChangeMask} value={value} />
+        </div>
+    );
+}
+
+export default MainOrganism;
--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { maskCPF } from "../utils/maskCPF";
 import TitlesMain from "../components/molecules/TitlesMain/TitlesMain";
 import { ButtonAndInput } from "../components/molecules/ButtonAndInput/ButtonAndInput";
+import MainOrganism from "../components/organisms/MainOrganism";
 
 function Main() {
     const [value, setValue] = useState("");
@@ -17,10 +18,7 @@ function Main() {
             <div className="bg-orange-500 w-screen h-screen fixed font-mono">
                 <main className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
                     <div className="mt-10 text-center text-gray-200 drop-shadow-lg">
-                        <div className="flex flex-col w-full h-full	items-center">
-                            <TitlesMain />
-                            <ButtonAndInput btnText="VAMOS LÁ!" handleOnChange={handleChangeMask} value={value} />
-                        </div>
+                        <MainOrganism />
                     </div>
                 </main>
             </div>
