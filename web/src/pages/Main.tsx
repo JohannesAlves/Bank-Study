5b774fa2128244898ad3b6905b376a42c0b7d646
JohannesAlves <johannes001002@gmail.com>
2022-12-01 21:30:08 -0300

    feat: main page init

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,7 +1,16 @@
 function Main() {
     return (
         <>
-            <h1>teste</h1>
+            <div className="bg-orange-500 w-screen h-screen fixed font-mono">
+                <main className="bg-zinc-800 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-xl ">
+                    <div className="mt-10 text-center">
+                        <h1 className="text-3xl  text-gray-200 drop-shadow-lg">
+                            👋 Olá, futuro parceiro(a) de equipe!
+                        </h1>
+                        <h2 className="text-3xl  text-gray-200 drop-shadow-lg mt-5">Me informe o seu CPF</h2>
+                    </div>
+                </main>
+            </div>
         </>
     );
 }
