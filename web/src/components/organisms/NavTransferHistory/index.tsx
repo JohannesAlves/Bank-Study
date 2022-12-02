375a16a2ce82ee135c377df7812ff2d7f483ee1b
JohannesAlves <johannes001002@gmail.com>
2022-12-02 14:55:30 -0300

    feat: nav transfer history label

---
+++ web/src/components/organisms/NavTransferHistory/index.tsx
@@ -0,0 +1,13 @@
+import { Subtitle } from "../../atoms/Subtitle";
+
+export function NavTransferHistory() {
+    return (
+        <>
+            <div className="w-9/12 h-4/5 bg-slate-200 mx-auto mt-10 rounded-lg flex justify-center">
+                <div>
+                    <Subtitle subtitle="HISTÓRICO" />
+                </div>
+            </div>
+        </>
+    );
+}
--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,12 +1,13 @@
 import { NavIndex } from "../components/organisms/NavIndex";
 import { NavOrganism } from "../components/organisms/NavOrganism";
+import { NavTransferHistory } from "../components/organisms/NavTransferHistory";
 
 function Profile() {
     return (
         <>
             <div className="flex flex-row h-full">
                 <NavOrganism />
-                <NavIndex />
+                <NavTransferHistory />
             </div>
         </>
     );
