3d045608a9466ca76f4edf4a9b5cd24a71c738f3
JohannesAlves <johannes001002@gmail.com>
2022-12-02 00:41:15 -0300

    chore: add atoms title & subtitle

---
+++ web/src/components/atoms/subtitle/index.tsx
@@ -0,0 +1,5 @@
+function Subtitle(subtitle: string) {
+    return <h2 className="text-3xl mt-5">{subtitle}</h2>;
+}
+
+export { Subtitle };
---
+++ web/src/components/atoms/title/index.tsx
@@ -0,0 +1,5 @@
+function Title(title: string) {
+    return <h1 className="text-3xl">{title}</h1>;
+}
+
+export { Title };
