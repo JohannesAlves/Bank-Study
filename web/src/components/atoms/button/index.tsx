f4c1890417d9c9d799f5a33b6cf2fc38e0e1bd49
JohannesAlves <johannes001002@gmail.com>
2022-12-02 11:29:22 -0300

    refactor: color btn

--- web/src/components/atoms/button/index.tsx
+++ web/src/components/atoms/button/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 function Button({ btnText }: Props) {
     return (
         <>
-            <a className="border-2 mt-10  border-orange-600 rounded-2xl px-3 py-2 text-orange-600 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold">
+            <a className="border-2 mt-10  border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold">
                 {btnText}
             </a>
         </>
