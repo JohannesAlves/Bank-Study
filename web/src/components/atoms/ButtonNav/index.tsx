d2941bff56013c2b836025783c573dcd7e76aba5
JohannesAlves <johannes001002@gmail.com>
2022-12-02 19:45:20 -0300

    fix: cached files

--- web/src/components/atoms/ButtonNav/index.tsx
+++
@@ -1,9 +0,0 @@
-type TButtonNavProps = {
-    btnText: string;
-};
-
-function ButtonNav({ btnText }: TButtonNavProps) {
-    return <button className="hover:bg-orange-500 rounded">{btnText}</button>;
-}
-
-export default ButtonNav;
--- web/src/components/atoms/title/index.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-    title: string;
-};
-
-function Title({ title }: Props) {
-    return <h1 className="text-3xl">{title}</h1>;
-}
-
-export { Title };
