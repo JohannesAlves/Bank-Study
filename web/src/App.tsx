c1bd2694969f2ede33bea8671b6495f57ecbfa97
JohannesAlves <johannes001002@gmail.com>
2022-12-02 20:55:27 -0300

    feat: error 404 page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { Error404 } from "./pages/Error404";
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
 import { Signup } from "./pages/Signup";
@@ -6,7 +7,7 @@ function App() {
     return (
         <div className="overflow-auto bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
             <div className="bg-zinc-800  h-full lg:h-2/4  w-2/3 lg:w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
-                <Signup />
+                <Error404 />
             </div>
         </div>
     );
---
+++ web/src/pages/Error404.tsx
@@ -0,0 +1,9 @@
+export function Error404() {
+    return (
+        <div className="h-full w-full">
+            <div className="flex justify-center">
+                <h1 className="text-6xl text-slate-300 mt-32">Error 404 ;(</h1>
+            </div>
+        </div>
+    );
+}
