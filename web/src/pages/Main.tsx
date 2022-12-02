66b48d69a5211b8b3684e4b188101e9f3d0740e3
JohannesAlves <johannes001002@gmail.com>
2022-12-02 00:22:04 -0300

    chore: add mask cpf.

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import woman from "../assets/woman.svg";
+import { maskCPF } from "../utils/maskCpf";
 
 function Main() {
+    const [value, setValue] = useState("");
+
+    function handleChangeMask(event) {
+        const { value } = event.target;
+
+        setValue(maskCPF(value));
+    }
+
     return (
         <>
-            <div className="bg-gradient-to-r from-orange-500 to-stone-900 w-screen h-screen fixed font-mono">
-                <main className="bg-zinc-800  bg-opacity-50 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
+            <div className="bg-orange-500 w-screen h-screen fixed font-mono">
+                <main className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
                     <div className="mt-10 text-center text-gray-200 drop-shadow-lg">
                         <div>
                             <h1 className="text-3xl">👋 Olá, futuro parceiro(a) de equipe!</h1>
@@ -15,6 +25,8 @@ function Main() {
                             <input
                                 type="text"
                                 name="name"
+                                onChange={handleChangeMask}
+                                value={value}
                                 placeholder="123.456.789-10"
                                 className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
                             />
