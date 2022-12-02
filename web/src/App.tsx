4922b12805acb4ad46225bbc6d701fed20f8bfd8
JohannesAlves <johannes001002@gmail.com>
2022-12-02 11:42:31 -0300

    refactor: remove background & square black from main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import Main from "./pages/Main";
 
 function App() {
-    return <Main />;
+    return (
+        <div className="bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
+            <div className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
+                <Main />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -3,12 +3,8 @@ import MainOrganism from "../components/organisms/MainOrganism/MainOrganism";
 function Main() {
     return (
         <>
-            <div className="bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
-                <main className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36 flex justify-center rounded-lg shadow-lg">
-                    <div className="mt-10 text-center text-gray-200 drop-shadow-lg">
-                        <MainOrganism />
-                    </div>
-                </main>
+            <div className="mt-10 text-center text-gray-200 drop-shadow-lg">
+                <MainOrganism />
             </div>
         </>
     );
