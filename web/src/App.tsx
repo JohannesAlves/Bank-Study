db7f8984126347f43ad830d737b7287e4555e8b2
JohannesAlves <johannes001002@gmail.com>
2022-12-02 15:45:30 -0300

    refactor: change app background color.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import Profile from "./pages/Profile";
 function App() {
     return (
         <div className="bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
-            <div className="bg-zinc-800  bg-opacity-60 h-2/4  w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
+            <div className="bg-zinc-800  h-2/4  w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
                 <Profile />
             </div>
         </div>
