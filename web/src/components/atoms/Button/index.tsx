19e283b0a3e86ec8b4c45e3a6eb74d9dbda22db1
JohannesAlves <johannes001002@gmail.com>
2022-12-03 11:20:49 -0300

    refactor: change to btn.

--- web/src/components/atoms/Button/index.tsx
+++ web/src/components/atoms/Button/index.tsx
@@ -5,9 +5,9 @@ type Props = {
 function Button({ btnText }: Props) {
     return (
         <>
-            <a className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold">
+            <button className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold">
                 {btnText}
-            </a>
+            </button>
         </>
     );
 }
