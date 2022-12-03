5e5552adb9fe1d7f845a58850649d32e8dc3a874
JohannesAlves <johannes001002@gmail.com>
2022-12-03 18:01:37 -0300

    chore: get transfers controller.

--- server/src/controllers/TransferController.ts
+++ server/src/controllers/TransferController.ts
@@ -81,4 +81,25 @@ const TransferController = async (request: Request, response: Response) => {
     }
 };
 
-export default TransferController;
+const GetTransfersController = async (request: Request, response: Response) => {
+    const { userAccount } = response.locals;
+
+    try {
+        const transfersUser = await prisma.transfer.findMany({
+            where: {
+                fromAccountId: userAccount.accountId,
+            },
+            select: {
+                amount: true,
+                idTransfer: true,
+                createdAt: true,
+            },
+        });
+
+        return response.status(200).json({ transfersUser });
+    } catch {
+        return response.status(500).json({ message: "Internal server error to get transfers" });
+    }
+};
+
+export { TransferController, GetTransfersController };
--- server/src/routes/Transfer.routes.ts
+++ server/src/routes/Transfer.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import TransferController from "../controllers/TransferController";
+import { GetTransfersController, TransferController } from "../controllers/TransferController";
 import { auth } from "../middleware/auth";
 import getUserAccount from "../middleware/getUserAccount";
 import { verifyBalanceAccount } from "../middleware/verifyBalanceAccount";
 const TransferRoutes = Router();
 
 TransferRoutes.post("/transfer", auth, getUserAccount, verifyBalanceAccount, TransferController);
+TransferRoutes.get("/transfer", auth, getUserAccount, GetTransfersController);
 
 export default TransferRoutes;
