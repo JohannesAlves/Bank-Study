5d528a5f421286157b0f929136718e75e40dddc8
JohannesAlves <johannes001002@gmail.com>
2022-12-03 12:23:14 -0300

    refactor: change input to normal input.

--- web/src/pages/Signup.tsx
+++ web/src/pages/Signup.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { Button } from "../components/atoms/Button";
-import Input from "../components/atoms/Input";
 import { maskCPF } from "../utils/maskCPF";
+import Input from "../components/atoms/Input";
+import { useForm } from "react-hook-form";
 
 export function Signup() {
     const [value, setValue] = useState("");
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data);
 
     function handleChangeMask(event: React.FormEvent<HTMLInputElement>) {
         const { value } = event.currentTarget;
@@ -17,21 +26,29 @@ export function Signup() {
             <div className="flex flex-col text-center text-slate-300  w-full h-full">
                 <h2 className="text-2xl font-mono mt-10">Hmmm, parece que você ainda não é registrado.</h2>
 
-                <div className="flex flex-row  justify-center space-x-20 mt-10">
-                    <div>
-                        <h2>Qual é o seu nome completo?</h2>
-                        <Input placeholder="Nome completo..." />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-row  justify-center space-x-20 mt-10">
+                        <div>
+                            <h2>Qual é o seu nome completo?</h2>
+                            <input
+                                placeholder="Nome completo..."
+                                className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
+                            />
+                        </div>
+
+                        <div>
+                            <h2>Me informa um CPF válido</h2>
+                            <input
+                                placeholder="123.456.178-12"
+                                className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
+                            />
+                        </div>
                     </div>
 
-                    <div>
-                        <h2>Me informa um CPF válido</h2>
-                        <Input placeholder="123.456.178-12" handleOnChange={handleChangeMask} value={value} />
+                    <div className="mt-20">
+                        <Button btnText="Cadastrar" />
                     </div>
-                </div>
-
-                <div className="mt-20">
-                    <Button btnText="Cadastrar" />
-                </div>
+                </form>
             </div>
         </>
     );
