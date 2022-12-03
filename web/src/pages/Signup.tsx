cd05c7974bd8d6a3d6c086dc176fbf257dc855e3
JohannesAlves <johannes001002@gmail.com>
2022-12-03 12:30:54 -0300

    refactor: types in form & add mask cpf

--- web/src/pages/Signup.tsx
+++ web/src/pages/Signup.tsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import { Button } from "../components/atoms/Button";
 import { maskCPF } from "../utils/maskCPF";
 import Input from "../components/atoms/Input";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormValues {
+    cpf: string;
+    fullname: string;
+}
 
 export function Signup() {
     const [value, setValue] = useState("");
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm();
+    const { register, handleSubmit } = useForm<FormValues>();
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
     function handleChangeMask(event: React.FormEvent<HTMLInputElement>) {
         const { value } = event.currentTarget;
@@ -31,6 +31,7 @@ export function Signup() {
                         <div>
                             <h2>Qual é o seu nome completo?</h2>
                             <input
+                                {...register("fullname")}
                                 placeholder="Nome completo..."
                                 className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
                             />
@@ -39,6 +40,10 @@ export function Signup() {
                         <div>
                             <h2>Me informa um CPF válido</h2>
                             <input
+                                autoComplete="off"
+                                {...register("cpf")}
+                                onChange={handleChangeMask}
+                                value={value}
                                 placeholder="123.456.178-12"
                                 className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
                             />
