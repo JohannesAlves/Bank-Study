e2c86825a8e69071edd9e6ba69a7c89845216549
JohannesAlves <johannes001002@gmail.com>
2022-12-03 14:27:26 -0300

    chore: add link in btn register.

--- web/src/pages/Main/Main.tsx
+++ web/src/pages/Main/Main.tsx
@@ -4,6 +4,8 @@ import { TitlesMain } from "../../components/molecules/TitlesMain";
 import { Button } from "../../components/atoms/Button";
 import { AuthContext } from "../../context/AuthContext";
 import { SubmitHandler, useForm } from "react-hook-form";
+import ButtonNav from "../../components/atoms/ButtonNav";
+import { Link } from "react-router-dom";
 
 interface FormValues {
     cpf: string;
@@ -40,14 +42,22 @@ function Main() {
                 <div className="flex flex-col w-full h-full	items-center">
                     <TitlesMain />
 
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input
-                            {...register("cpf")}
-                            onChange={handleChangeMask}
-                            value={value}
-                            className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
-                        />
-                        <Button btnText="VAMOS LÁ!" />
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+                        <div>
+                            <input
+                                {...register("cpf")}
+                                onChange={handleChangeMask}
+                                value={value}
+                                className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
+                            />
+                        </div>
+
+                        <div className="space-x-5">
+                            <Button btnText="VAMOS LÁ!" />
+                            <Link to="/signup">
+                                <Button btnText="REGISTRAR" />
+                            </Link>
+                        </div>
                     </form>
                 </div>
             </div>
