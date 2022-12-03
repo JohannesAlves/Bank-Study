29186a3fed8f237514ba24b4afddb46ad9dd3324
JohannesAlves <johannes001002@gmail.com>
2022-12-02 22:20:45 -0300

    chore: add routes.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,19 @@ import { Error404 } from "./pages/Error404";
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
 import { Signup } from "./pages/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="overflow-auto bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
             <div className="bg-zinc-800  h-full lg:h-2/4  w-2/3 lg:w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
-                <Error404 />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
         </div>
     );
--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,4 +1,6 @@
-import { NavIndex } from "../components/organisms/ProfileIndex";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { ProfileIndex } from "../components/organisms/ProfileIndex";
 import { Nav } from "../components/organisms/Nav";
 import { ProfileHistoric } from "../components/organisms/ProfileHistoric";
 import { ProfileDeposit } from "../components/organisms/ProfileDeposit";
@@ -9,7 +11,12 @@ function Profile() {
         <>
             <div className="flex flex-col lg:flex-row h-full font-sans">
                 <Nav />
-                <ProfileTransfer />
+                <Routes>
+                    <Route path="/index" element={<ProfileIndex />} />
+                    <Route path="/historic" element={<ProfileHistoric />} />
+                    <Route path="/deposit" element={<ProfileDeposit />} />
+                    <Route path="/transfer" element={<ProfileTransfer />} />
+                </Routes>
             </div>
         </>
     );
