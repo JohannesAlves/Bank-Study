74d1522c7e2ac74537c79ceabff1e33238649eb5
JohannesAlves <johannes001002@gmail.com>
2022-12-04 20:07:02 -0300

    refactor: remove unusual code.

--- server/src/routes/Balance.routes.ts
+++ server/src/routes/Balance.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import BalanceController from "../controllers/BalanceController";
 import { auth } from "../middleware/auth";
-import { verifyIfAccountExist } from "../middleware/verifyIfAccountExist";
 
 const BalanceRoutes = Router();
 
