957c55d87782c6fc7e273fc4a105f07d4bddcac5
JohannesAlves <johannes001002@gmail.com>
2022-12-04 20:54:04 -0300

    refactor: date format.

--- web/src/components/organisms/ProfileHistoric/Transfers.tsx
+++ web/src/components/organisms/ProfileHistoric/Transfers.tsx
@@ -8,11 +8,17 @@ export function Transfers() {
     }
 
     return data?.transfersUser?.map((transferUser, index) => {
+        const date = new Date(transferUser.createdAt);
+        const dateYear = date.getFullYear();
+        const dateMonth = date.getMonth();
+        const dateDay = date.getDate();
+        const transferDate = `${dateDay}/${dateMonth}/${dateYear}`;
+
         return (
             <>
                 <tr className="bg-gray-600 text-slate-200" key={transferUser.idTransfer}>
                     <td>Transferência</td>
-                    <td>{transferUser.createdAt}</td>
+                    <td>{transferDate}</td>
                     <td>R${transferUser.amount}</td>
                 </tr>
             </>
