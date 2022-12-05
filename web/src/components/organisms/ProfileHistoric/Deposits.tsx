4e9ea8699d3f16c9cac1c3b05b7f45752771cf3a
JohannesAlves <johannes001002@gmail.com>
2022-12-05 11:15:38 -0300

    chore: receive deposits data in table historic.

--- web/src/components/organisms/ProfileHistoric/Deposits.tsx
+++ web/src/components/organisms/ProfileHistoric/Deposits.tsx
@@ -1,27 +1,34 @@
 import { TwoDecimalsNumber } from "../../../utils/2DecimalsNumber";
+import TableHistoric from "../TableHistoric/TableHistoric";
 import { useHistoric } from "./useHistoric";
 
 export function Deposits() {
     const data = useHistoric();
 
+    function DepositsData() {
+        return (
+            <>
+                {data?.depositsUser?.map((depositUser, index) => {
+                    const date = new Date(depositUser.createdAt);
+                    const transferDate = new Intl.DateTimeFormat("pt-BR").format(date);
+                    const amountToNumber = Number(depositUser.amount);
+                    return (
+                        <>
+                            <tr className="bg-gray-600 text-slate-200" key={depositUser.idDeposit}>
+                                <td>Depósitos</td>
+                                <td>{transferDate}</td>
+                                <td>R${TwoDecimalsNumber(amountToNumber)}</td>
+                            </tr>
+                        </>
+                    );
+                })}
+            </>
+        );
+    }
+
     return (
         <>
-            {data?.depositsUser?.map((depositUser, index) => {
-                const date = new Date(depositUser.createdAt);
-                const transferDate = new Intl.DateTimeFormat("pt-BR").format(date);
-
-                const amountToNumber = Number(depositUser.amount);
-
-                return (
-                    <>
-                        <tr className="bg-gray-600 text-slate-200" key={depositUser.idDeposit}>
-                            <td>Depósitos</td>
-                            <td>{transferDate}</td>
-                            <td>R${TwoDecimalsNumber(amountToNumber)}</td>
-                        </tr>
-                    </>
-                );
-            })}
+            <TableHistoric view={<DepositsData />} />
         </>
     );
 }
