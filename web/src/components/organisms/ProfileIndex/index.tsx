837f57804c758b0ab4db09ce0b9fa10aaf044981
JohannesAlves <johannes001002@gmail.com>
2022-12-05 18:40:11 -0300

    refactor: balance to currency money.

--- web/src/components/organisms/ProfileIndex/index.tsx
+++ web/src/components/organisms/ProfileIndex/index.tsx
@@ -4,12 +4,15 @@ import { useBalance } from "./useBalance";
 export function ProfileIndex() {
     const data = useBalance();
     const balanceToNumber = Number(data.balance);
+    const balanceToDecimal = Number(TwoDecimalsNumber(balanceToNumber));
+
+    const decimalToCurrencyMoney = balanceToDecimal.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 
     return (
         <div className="mx-auto mt-20 text-4xl text-center">
             <h3 className="text-slate-200">
                 Saldo disponível: <br />
-                R${TwoDecimalsNumber(balanceToNumber)}
+                {decimalToCurrencyMoney}
             </h3>
         </div>
     );
