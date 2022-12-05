7be7bc5b1a55d6ed080d50d000e64cfeb4424167
JohannesAlves <johannes001002@gmail.com>
2022-12-04 21:01:12 -0300

    chore: add func to transform balance to 2 decimals.

--- web/src/components/organisms/ProfileIndex/index.tsx
+++ web/src/components/organisms/ProfileIndex/index.tsx
@@ -1,13 +1,15 @@
+import { TwoDecimalsNumber } from "../../../utils/2DecimalsNumber";
 import { useBalance } from "./useBalance";
 
 export function ProfileIndex() {
     const data = useBalance();
+    const balanceToNumber = Number(data.balance);
 
     return (
         <div className="mx-auto mt-20 text-4xl text-center">
             <h3 className="text-slate-200">
                 Saldo disponível: <br />
-                R${data.balance}
+                R${TwoDecimalsNumber(balanceToNumber)}
             </h3>
         </div>
     );
