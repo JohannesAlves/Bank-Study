e4c942ef4fd1fb3617ba688e9ea38be147d4bab5
JohannesAlves <johannes001002@gmail.com>
2022-12-04 21:48:43 -0300

    refactor: remove use deposit and use inside page.

--- web/src/components/organisms/ProfileDeposit/index.tsx
+++ web/src/components/organisms/ProfileDeposit/index.tsx
@@ -1,9 +1,29 @@
 import { Button } from "../../atoms/Button";
 import Input from "../../atoms/Input";
 import requestMoney from "../../../assets/requestMoney.png";
-import { useDeposit } from "./useDeposit";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { api } from "../../../api/api";
+
+interface FormValues {
+    amount: string;
+}
 
 export function ProfileDeposit() {
+    const { register, handleSubmit } = useForm<FormValues>();
+    const { user } = useContext(AuthContext);
+
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
+        const amountToNumber = Number(data.amount);
+
+        try {
+            const response = await api.post("/deposit", { amount: amountToNumber });
+        } catch {
+            return alert("Error in deposit.");
+        }
+    };
+
     return (
         <>
             <div className=" w-9/12 h-4/3 mx-auto mt-10 rounded-md overflow-auto">
@@ -13,22 +33,25 @@ export function ProfileDeposit() {
                         <img src={requestMoney} className="w-12" />
                     </div>
 
-                    <div className="flex flex-col items-center">
-                        <input
-                            placeholder="R$100,00"
-                            className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
-                        />
-                        <p className="text-slate-600 mt-1">O depósito máximo é de R$2000,00 ;)</p>
-                    </div>
-                    <div className="mt-1 flex justify-center flex-wrap space-x-2 lg:space-x-6">
-                        <Button btnText="R$100,00" />
-                        <Button btnText="R$200,00" />
-                        <Button btnText="R$300,00" />
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="flex flex-col items-center">
+                            <input
+                                {...register("amount")}
+                                placeholder="R$100,00"
+                                className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-3xl text-center"
+                            />
+                            <p className="text-slate-600 mt-1">O depósito máximo é de R$2000,00 ;)</p>
+                        </div>
+                        <div className="mt-1 flex justify-center flex-wrap space-x-2 lg:space-x-6">
+                            <Button btnText="R$100,00" />
+                            <Button btnText="R$200,00" />
+                            <Button btnText="R$300,00" />
+                        </div>
 
-                    <div className="flex justify-center">
-                        <Button btnText="DEPOSITAR" />
-                    </div>
+                        <div className="flex justify-center">
+                            <Button btnText="DEPOSITAR" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
