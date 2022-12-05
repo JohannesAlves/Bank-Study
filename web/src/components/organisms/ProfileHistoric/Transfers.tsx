a46a759396631fb6a2a7def54e37e659550ab350
JohannesAlves <johannes001002@gmail.com>
2022-12-05 11:17:54 -0300

    refactor: transfers receive table historic.

--- web/src/components/organisms/ProfileHistoric/Transfers.tsx
+++ web/src/components/organisms/ProfileHistoric/Transfers.tsx
@@ -1,4 +1,5 @@
 import { TwoDecimalsNumber } from "../../../utils/2DecimalsNumber";
+import TableHistoric from "../TableHistoric/TableHistoric";
 import { useHistoric } from "./useHistoric";
 
 export function Transfers() {
@@ -8,24 +9,30 @@ export function Transfers() {
         return <h1>Sem dados</h1>;
     }
 
+    function TransfersData() {
+        return (
+            <>
+                {data?.transfersUser?.map((transferUser, index) => {
+                    const date = new Date(transferUser.createdAt);
+                    const depositDate = new Intl.DateTimeFormat("pt-BR").format(date);
+                    const amountToNumber = Number(transferUser.amount);
+                    return (
+                        <>
+                            <tr className="bg-gray-600 text-slate-200" key={transferUser.idTransfer}>
+                                <td>Depósitos</td>
+                                <td>{depositDate}</td>
+                                <td>R${TwoDecimalsNumber(amountToNumber)}</td>
+                            </tr>
+                        </>
+                    );
+                })}
+            </>
+        );
+    }
+
     return (
         <>
-            {data?.transfersUser?.map((transferUser, index) => {
-                const date = new Date(transferUser.createdAt);
-                const transferDate = new Intl.DateTimeFormat("pt-BR").format(date);
-
-                const amountToNumber = Number(transferUser.amount);
-
-                return (
-                    <>
-                        <tr className="bg-gray-600 text-slate-200" key={transferUser.idTransfer}>
-                            <td>Transferência</td>
-                            <td>{transferDate}</td>
-                            <td>R${TwoDecimalsNumber(amountToNumber)}</td>
-                        </tr>
-                    </>
-                );
-            })}
+            <TableHistoric view={<TransfersData />} />
         </>
     );
 }
