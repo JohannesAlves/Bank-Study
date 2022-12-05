76bf669cbc84c653eb09bd321de3cd33f65d0514
JohannesAlves <johannes001002@gmail.com>
2022-12-05 19:25:24 -0300

    chore: transfer page now post data to api.

--- web/src/components/organisms/ProfileTransfer/index.tsx
+++ web/src/components/organisms/ProfileTransfer/index.tsx
@@ -1,8 +1,46 @@
 import { Button } from "../../atoms/Button";
 import Input from "../../atoms/Input";
 import moneyTransfer from "../../../assets/moneyTransfer.png";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { api } from "../../../api/api";
+import { TwoDecimalsNumber } from "../../../utils/2DecimalsNumber";
+
+interface FormValues {
+    amount: string;
+    toAccountId: string;
+}
 
 export function ProfileTransfer() {
+    const { register, handleSubmit, setValue } = useForm<FormValues>();
+    const { user } = useContext(AuthContext);
+    const [amount, setAmount] = useState("R$0.00");
+
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
+        const amountToNumber = Number(data.amount);
+        const amountToDecimal = Number(TwoDecimalsNumber(amountToNumber));
+        const toAccountId = data.toAccountId;
+
+        if (amountToDecimal > 2000) {
+            alert("Ei, esqueceu do combinado? No máximo R$2000,00");
+            return;
+        }
+
+        try {
+            if (user.fullname) {
+                const response = await api.post("/transfer", { amount: amountToDecimal, toAccountId });
+                if (response) {
+                    alert("Transferência realizada com sucesso!");
+                }
+            } else {
+                return;
+            }
+        } catch {
+            return alert("Error in transfer.");
+        }
+    };
+
     return (
         <>
             <div className=" w-9/12 h-4/3 mx-auto mt-10 rounded-md overflow-auto">
@@ -12,19 +50,72 @@ export function ProfileTransfer() {
                         <img src={moneyTransfer} className="w-12" />
                     </div>
 
-                    <div className="flex flex-col items-center">
-                        <Input placeholder="R$150,00" />
-                        <p className="text-slate-600 mt-1">O máximo de transferência é R$2000,00 ;)</p>
-                    </div>
-                    <div className="mt-1 flex justify-center flex-wrap space-x-2 lg:space-x-6">
-                        <Button btnText="R$100,00" />
-                        <Button btnText="R$200,00" />
-                        <Button btnText="R$300,00" />
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="flex flex-col items-center">
+                            <div className="flex flex-row space-x-10 flex-wrap justify-center">
+                                <input
+                                    {...(register("amount"),
+                                    {
+                                        onChange: (event) => {
+                                            const { value } = event.currentTarget;
+                                            const removedCurrencyMask = value.slice(2);
+                                            const valueToDecimal = Number(removedCurrencyMask).toFixed(2);
+                                            setValue("amount", `${valueToDecimal}`);
+                                            setAmount(`R$${valueToDecimal}`);
+                                        },
+                                        value: amount,
+                                    })}
+                                    placeholder="Qual será o valor?"
+                                    className="w-64 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-2xl text-center"
+                                />
 
-                    <div className="flex justify-center">
-                        <Button btnText="TRANSFERIR" />
-                    </div>
+                                <input
+                                    {...register("toAccountId")}
+                                    placeholder="Para qual conta?"
+                                    className="w-96 h-11 px-4 py-2 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 text-2xl text-center"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-1 flex justify-center flex-wrap space-x-2 lg:space-x-6">
+                            <button
+                                onClick={() => {
+                                    const value = 100;
+                                    setValue("amount", `${value}`);
+                                    setAmount(`R$100.00`);
+                                }}
+                                type="button"
+                                className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold"
+                            >
+                                R$100,00
+                            </button>
+                            <button
+                                onClick={(event) => {
+                                    const value = 200;
+                                    setValue("amount", `${value}`);
+                                    setAmount(`R$200.00`);
+                                }}
+                                type="button"
+                                className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold"
+                            >
+                                R$200,00
+                            </button>
+                            <button
+                                onClick={(event) => {
+                                    const value = 300;
+                                    setValue("amount", `${value}`);
+                                    setAmount(`R$300.00`);
+                                }}
+                                type="button"
+                                className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold"
+                            >
+                                R$300,00
+                            </button>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <Button btnText="TRANSFERIR" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
