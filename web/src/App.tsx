b5dbee95b5b0e251271f0552099c0d11295a5e5a
JohannesAlves <johannes001002@gmail.com>
2022-12-06 19:33:18 -0300

    refactor: height main in mobile.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,7 +9,7 @@ import { ProtectedRoute } from "./components/organisms/ProtectedRoutes/Protected
 function App() {
     return (
         <div className="overflow-auto bg-gradient-to-br from-orange-500 to-orange-700 w-screen h-screen fixed font-mono">
-            <div className="bg-zinc-800  h-full lg:h-2/4  w-2/3 lg:w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
+            <div className="bg-zinc-800   h-2/4  w-2/3 lg:w-2/4 mx-auto mt-36  rounded-lg shadow-lg">
                 <BrowserRouter>
                     <AuthProvider>
                         <Routes>
