3b448082bd2cb2737f422c4a4676cdc0aefcf753
JohannesAlves <johannes001002@gmail.com>
2022-12-06 17:41:59 -0300

    chore: responsive btns historic.

--- web/src/components/organisms/ProfileHistoric/index.tsx
+++ web/src/components/organisms/ProfileHistoric/index.tsx
@@ -7,7 +7,7 @@ export function ProfileHistoric() {
 
     return (
         <>
-            <div className="mx-auto mt-32 space-x-6">
+            <div className="flex justify-center mx-auto mt-32 space-x-3 2xl:space-x-6 flex-wrap">
                 <Link to="deposits">
                     <button className="border-2 mt-10 hover:shadow-lg transition-all duration-300	 hover:shadow-orange-500/50 border-orange-500 rounded-2xl px-3 py-2 text-orange-500 cursor-pointer hover:bg-stone-900 hover:text-gray-200 font-bold">
                         DEPÓSITOS
