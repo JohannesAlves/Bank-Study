7252d44d7b21359611db0493bcdcb3c4cb7c6cf8
JohannesAlves <johannes001002@gmail.com>
2022-12-06 20:05:01 -0300

    refactor: responsive btns historic.

--- web/src/components/organisms/ProfileHistoric/index.tsx
+++ web/src/components/organisms/ProfileHistoric/index.tsx
@@ -7,13 +7,13 @@ export function ProfileHistoric() {
 
     return (
         <>
-            <div className="flex justify-center mx-auto mt-32 space-x-3 2xl:space-x-6 flex-wrap">
+            <div className="flex justify-center mx-auto lg:mt-32 min-[427px]:space-x-3  2xl:space-x-6 flex-wrap  h-96">
                 <Link to="deposits">
                     <button className="btn-variant-one mt-10">DEPÓSITOS</button>
                 </Link>
 
                 <Link to="transfers">
-                    <button className="btn-variant-one mt-10 w-40">TRANSFERÊNCIAS</button>
+                    <button className="btn-variant-one min-[409px]:mt-10 w-40">TRANSFERÊNCIAS</button>
                 </Link>
             </div>
         </>
