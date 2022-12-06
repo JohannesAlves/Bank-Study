eb7edd1a7e20dbb966dfe4e24a7756d6c353b00c
JohannesAlves <johannes001002@gmail.com>
2022-12-06 19:39:58 -0300

    refactor: signup responsive and change to flex row.

--- web/src/pages/Signup/Signup.tsx
+++ web/src/pages/Signup/Signup.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Button } from "../../components/atoms/Button";
 import { maskCPF } from "../../utils/maskCPF";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { api } from "../../api/api";
@@ -35,15 +34,15 @@ export function Signup() {
 
     return (
         <>
-            <div className="flex flex-col justify-center text-center text-slate-300  2xl:w-full 2xl:h-full flex-wrap">
+            <div className="flex flex-row justify-center text-center text-slate-300  2xl:w-full 2xl:h-full flex-wrap ">
                 <div>
-                    <h2 className="text-xl 2xl:text-2xl font-mono mt-10">
+                    <h2 className="text-xl 2xl:text-2xl font-mono mt-10 ">
                         Hmmm, parece que você ainda não é registrado.
                     </h2>
                 </div>
 
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex flex-col mt-10 items-center">
+                    <div className="flex sm:space-x-5 2xl:space-x-10 flex-wrap justify-center">
                         <div>
                             <h2>Qual é o seu nome completo?</h2>
                             <input
@@ -53,23 +52,21 @@ export function Signup() {
                             />
                         </div>
 
-                        <div className="flex flex-col justify-center">
-                            <div className="mt-5">
-                                <h2>Me informa um CPF válido</h2>
-                            </div>
+                        <div className="">
+                            <h2 className="">Me informa um CPF válido</h2>
                             <input
                                 autoComplete="off"
                                 {...register("cpf")}
                                 onChange={handleChangeMask}
                                 value={value}
                                 placeholder="123.456.178-12"
-                                className="2xl:w-96 h-7 border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 lg:text-3xl text-lg text-center"
+                                className=" h-7  border-b-2 mt-14 bg-transparent border-gray-600 outline-none  focus:border-orange-500 text-gray-200 lg:text-3xl text-lg text-center"
                             />
                         </div>
+                    </div>
 
-                        <div className="">
-                            <button className="btn-variant-one mt-10">CADASTRAR</button>
-                        </div>
+                    <div className="">
+                        <button className="btn-variant-one  mt-5 sm:mt-10">CADASTRAR</button>
                     </div>
                 </form>
             </div>
