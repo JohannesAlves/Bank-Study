c92ad3ffda423a77ab614bca8a981ffe4015a0fe
JohannesAlves <johannes001002@gmail.com>
2022-12-07 11:52:13 -0300

    Revert "fix: input signup not allow digit."
    This reverts commit 13ce873695abd9dfcce370ccee1cb9b9cbb89eb0.

--- web/src/pages/Signup/Signup.tsx
+++ web/src/pages/Signup/Signup.tsx
@@ -33,7 +33,6 @@ export function Signup() {
                 navigate("/");
             }
         } catch (error) {
-            setIsLoading(false);
             alert("Algo de errado aconteceu!");
         }
     };
@@ -67,13 +66,8 @@ export function Signup() {
                                 autoComplete="off"
                                 {...(register("cpf"),
                                 {
-                                    maxLength: 14,
                                     minLength: 14,
-                                    required: true,
-                                    onChange: (event) => {
-                                        const { value } = event.currentTarget;
-                                        setValue("cpf", value);
-                                    },
+                                    maxLength: 14,
                                 })}
                                 placeholder="123.456.178-12"
                                 className="input-variant-one max-[615px]:w-44 max-[615px]:text-sm"
